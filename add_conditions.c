c02809a61e09dcd6b0ac838df50b425532d97393
matejkrenek <mate23.krenek@gmail.com>
2024-02-25 21:58:03 +0100

    implement other algoritms but fails

--- add_conditions.c
+++ add_conditions.c
@@ -18,15 +18,14 @@ void at_least_one_valid_street_for_each_step(CNF *formula, unsigned num_of_cross
     assert(num_of_streets > 0);
     assert(streets != NULL);
 
+    // ZDE PRIDAT KOD
     for (unsigned i = 0; i < num_of_streets; ++i)
     {
-        // pro kazdy krok i
+        Clause *cl = create_new_clause(formula);
 
         for (unsigned j = 0; j < num_of_streets; ++j)
         {
-            Clause *cl = create_new_clause(formula);
-
-            add_literal_to_clause(cl, streets[j].crossroad_from == i, i, streets[j].crossroad_from, streets[j].crossroad_to);
+            add_literal_to_clause(cl, true, i, streets[j].crossroad_from, streets[j].crossroad_to);
         }
     }
 }
@@ -41,6 +40,30 @@ void at_most_one_street_for_each_step(CNF *formula, unsigned num_of_crossroads, 
     assert(num_of_streets > 0);
 
     // ZDE PRIDAT KOD
+    for (unsigned i = 0; i < num_of_streets; ++i)
+    {
+        // pro kazdy krok cesty i
+        for (unsigned z1 = 0; z1 < num_of_crossroads; ++z1)
+        {
+            for (unsigned k1 = 0; k1 < num_of_crossroads; ++k1)
+            {
+                // pro kazdu dvojici krizovatek (z1, k1)
+                for (unsigned z2 = 0; z2 < num_of_crossroads; ++z2)
+                {
+                    for (unsigned k2 = 0; k2 < num_of_crossroads; ++k2)
+                    {
+                        if (z1 != z2 && k1 != k2)
+                        {
+                            // pro kazdu jinou dvojici krizovatek (z2, k2)
+                            Clause *cl = create_new_clause(formula);
+                            add_literal_to_clause(cl, false, i, z1, k1);
+                            add_literal_to_clause(cl, false, i, z2, k2);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 // Tato funkce by mela do formule pridat klauzule predstavujici podminku 3)
@@ -53,6 +76,25 @@ void streets_connected(CNF *formula, unsigned num_of_crossroads, unsigned num_of
     assert(num_of_streets > 0);
 
     // ZDE PRIDAT KOD
+    for (unsigned i = 0; i < num_of_streets - 1; ++i)
+    {
+        // pro kazdy krok cesty i
+        for (unsigned z = 0; z < num_of_crossroads; ++z)
+        {
+            for (unsigned k = 0; k < num_of_crossroads; ++k)
+            {
+                // pro kazdu dvojici krizovatek (z, k)
+                Clause *cl = create_new_clause(formula);
+                add_literal_to_clause(cl, false, i, z, k);
+
+                for (unsigned k2 = 0; k2 < num_of_crossroads; ++k2)
+                {
+                    // pro každou jinou končící křižovatku
+                    add_literal_to_clause(cl, true, i + 1, k, k2);
+                }
+            }
+        }
+    }
 }
 
 // Tato funkce by mela do formule pridat klauzule predstavujici podminku 4)
